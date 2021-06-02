5b5b4676d710b6319a323728b241adc2510b3efd
Aditya-A-garwal <aditya.agarwal030504@gmail.com>
2021-06-02 16:28:00 +0530

    Added filter to sort contours based on size and started implementing heirarchy based filtering

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -1,40 +1,63 @@
-import numpy as np
-import cv2
+# Resize image (pad if ratio not valid)
+
+# Go through image and find contours
+# Reject contours with areas too big or too small
+
+# Sort contours based on y coordinate into rows
+# Sort rows based on x coordinate into cols
+
+# Warp cropped image to match dimensions, label and store
+
+import cv2, numpy as np
+
+def check_area( contour ):
+    area = cv2.contourArea( contour )
+    if area < 20000: return 0
+    elif area < 30000: return 1
+    else: return 2
 
 # Load image and convert to grayscale (temporary for finding contours)
-img = cv2.imread('scan.jpg')
+img = cv2.imread('scanner\scan.jpg')
 imgGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Convert the image from grayscale to black and white by applying a threshold of 240
 _, thrash = cv2.threshold(imgGrey, 240, 255, cv2.THRESH_BINARY)
-contours, _ = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-# contours, _ = cv2.findContours(imgGrey, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+contours, hierarchy = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+hierarchy = hierarchy[0]
 
-# cv2.imshow("img", img)
+list_0 = [] # List of small contours
+list_1 = [] # list of correct contours
+list_2 = [] # List of big contours
 
-cntr = 0
+final_list = [list_0, list_1, list_2]
 
-for contour in contours:
+for i in range(len(contours)):
+    contour = contours[i]
+    heir    = hierarchy[i]
+
     approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
-    x = approx.ravel()[0] - 5
-    y = approx.ravel()[1] - 5
 
     if len(approx) == 4:
-        x1 ,y1, w, h = cv2.boundingRect(approx)
-        if w*h < 50 or w*h >= 1400*600: continue
-        aspectRatio = float(w)/h
-        print(aspectRatio)
-        cv2.putText(img, "rectangle", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
-        cv2.drawContours(img, [approx], 0, (0, 255, 0), 5)
-        cntr += 1
+        rect = cv2.boundingRect(contour)
+        box = cv2.minAreaRect(approx)
+        box = cv2.boxPoints(box)
+        box = np.int0(box)
 
-        # if aspectRatio >= 0.95 and aspectRatio <= 1.05:
-        # pass
-        # else:
-        # pass
+        final_list[check_area(contour)].append([i, rect, list(heir), cv2.contourArea(contour)])
 
+final_list[0].sort(key = lambda e : e[-1])
+final_list[1].sort(key = lambda e : e[-1])
+final_list[2].sort(key = lambda e : e[-1])
+
+for index, rect, heir, area in final_list[1]:
+    x, y, w, h = rect
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0,255,0), 1)
+    print(area, heir)
 
 cv2.imshow("shapes", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-print(cntr)
+
+print(len(final_list[0]))
+print(len(final_list[1]))
+print(len(final_list[2]))
