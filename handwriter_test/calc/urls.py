cbcce604c01a54df1e2b05888b79bbabc505f3a2
Aditya-A-garwal <aditya.agarwal030504@gmail.com>
2021-06-21 14:03:51 +0530

    fix: Uses correct set when selecting default
    Signed-off-by: Aditya-A-garwal <aditya.agarwal030504@gmail.com>

--- handwriter_test/calc/urls.py
+++ handwriter_test/calc/urls.py
@@ -7,6 +7,11 @@ urlpatterns = [
     path('', lambda req : render( req, "home.html" ), name='home'),
     path('add', views.add, name='add'),
     path('own_handwriting', lambda req : render( req, "io.html" ), name='own_handwriting'),
-    path('upload', views.upload, name='upload')
-
-] + [ path( 'h{}'.format(i), lambda req : views.hx( req, i ), name = 'h{}'.format(i) ) for i in range(1, 7) ]
+    path('upload', views.upload, name='upload'),
+    path('h1', lambda req : views.hx( req, 1 ), name = 'h1' ),
+    path('h2', lambda req : views.hx( req, 2 ), name = 'h2' ),
+    path('h3', lambda req : views.hx( req, 3 ), name = 'h3' ),
+    path('h4', lambda req : views.hx( req, 4 ), name = 'h4' ),
+    path('h5', lambda req : views.hx( req, 5 ), name = 'h5' ),
+    path('h6', lambda req : views.hx( req, 6 ), name = 'h6' )
+]
