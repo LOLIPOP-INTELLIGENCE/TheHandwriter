03610cd150929dfd3dad7c29645170aa34b827f5
Aditya-A-garwal <aditya.agarwal030504@gmail.com>
2021-06-23 16:28:10 +0530

    Generalized first and remaining cases in generate_word function

--- handwriter_test/calc/views.py
+++ handwriter_test/calc/views.py
@@ -126,7 +126,7 @@ def generate_word( _img_prev, _curr_word, _prev_exists, _num_spaces, _space_conc
 
     #Retrieving path of revelant character
     characters          = list(_curr_word )
-    character_first     = str( characters[0] )
+    # character_first     = str( characters[0] )
 
     special_dct     =   {'?':'question',
                         '!':'exclamation',
@@ -146,48 +146,49 @@ def generate_word( _img_prev, _curr_word, _prev_exists, _num_spaces, _space_conc
                         '\n':'blank2',
                         '~':'error'}
 
-    fil_name            = '{}.jpg'
+    # fil_name            = '{}.jpg'
 
-    if character_first.islower():      fil_name = fil_name.format( character_first + '_s' )
-    elif character_first.isupper():    fil_name = fil_name.format( character_first.lower() + '_b' )
-    elif character_first.isdigit():    fil_name = fil_name.format( character_first + '_d' )
-    else:                           fil_name = fil_name.format( special_dct.get( character_first, 'blank1' ) + '_x' )
+    # if character_first.islower():      fil_name = fil_name.format( character_first + '_s' )
+    # elif character_first.isupper():    fil_name = fil_name.format( character_first.lower() + '_b' )
+    # elif character_first.isdigit():    fil_name = fil_name.format( character_first + '_d' )
+    # else:                           fil_name = fil_name.format( special_dct.get( character_first, 'blank1' ) + '_x' )
 
 
-    #path holds the path to the first character in a word. If the word is Hello, 'path' is the path to 'H'
-    path = _base_path + fil_name
+    # #path holds the path to the first character in a word. If the word is Hello, 'path' is the path to 'H'
+    # path = _base_path + fil_name
 
-    #Read image
-    img = cv2.cvtColor( cv2.imread( path ), cv2.COLOR_BGR2GRAY )
+    # #Read image
+    # img = cv2.cvtColor( cv2.imread( path ), cv2.COLOR_BGR2GRAY )
 
-    # Convert all "greys" to black
-    black_low       = np.array( 0 )
-    black_high      = np.array( 50 )
-    mask            = cv2.inRange( img, black_low, black_high )
-    img[mask > 0]   = random.randint( 0, 50 )
+    # # Convert all "greys" to black
+    # black_low       = np.array( 0 )
+    # black_high      = np.array( 50 )
+    # mask            = cv2.inRange( img, black_low, black_high )
+    # img[mask > 0]   = random.randint( 0, 50 )
 
-    # Adding a white padding of 3 pixels to the left and right of the image
-    border = cv2.copyMakeBorder(
-        img,
-        top = 0, bottom = 0, left = 3, right = 3,
-        borderType = cv2.BORDER_CONSTANT,
-        value = (255, 255, 255)
-    )
+    # # Adding a white padding of 3 pixels to the left and right of the image
+    # border = cv2.copyMakeBorder(
+    #     img,
+    #     top = 0, bottom = 0, left = 3, right = 3,
+    #     borderType = cv2.BORDER_CONSTANT,
+    #     value = (255, 255, 255)
+    # )
 
-    # Adding the rotation
-    im_pil = Image.fromarray( border )
-    im_np = im_pil.rotate( random.randint( -5, 7 ), fillcolor = 'white' )
+    # # Adding the rotation
+    # im_pil = Image.fromarray( border )
+    # im_np = im_pil.rotate( random.randint( -5, 7 ), fillcolor = 'white' )
 
-    # Resizing the image and converting to numpy array so that we can concatenate images later on
-    im_np = np.asarray( im_np )
-    img = cv2.resize( im_np, (40, 114) )
+    # # Resizing the image and converting to numpy array so that we can concatenate images later on
+    # im_np = np.asarray( im_np )
+    # img = cv2.resize( im_np, (40, 114) )
+    img = None
 
     #doing the above for n-1 characters
-    for i in range( 1, len( characters ) ):
+    for i in range( len( characters ) ):
+
         characters_i    = str( characters[i] )
 
         fil_name        = '{}.jpg'
-
         if characters_i.islower():      fil_name = fil_name.format( characters_i + '_s' )
         elif characters_i.isupper():    fil_name = fil_name.format( characters_i.lower() + '_b' )
         elif characters_i.isdigit():    fil_name = fil_name.format( characters_i + '_d' )
@@ -214,16 +215,8 @@ def generate_word( _img_prev, _curr_word, _prev_exists, _num_spaces, _space_conc
         im_pil = Image.fromarray( border )
         im_np = np.asarray( im_pil.rotate( random.randint( -5, 7 ), fillcolor = 'white' ) )
         img2 = cv2.resize( im_np, (40, 114) )
-        img = np.concatenate((img, img2), axis=1)
+        img = np.concatenate( (img, img2), axis = 1 ) if img else img2
 
-
-    # This is there to add the image of previous words from _img_prev
-    # Decide wether to add the previous part of the sentence to this word
-    # For example, let the sentence be - "Hello there my name is Teddy"
-    # After generating "Hello", k will be false since we don't have a previous image
-    # After generating "there", k will be true and we will concatenate _img_prev to final_img
-    # Similarly, after generating "my", k will be true and the image for "Hello there"(stored in _img_prev)
-    # will be concatenated with final_img
     if (_prev_exists):
         final_img = np.concatenate((_img_prev, img), axis=1)
     if(_add_blank and _space_concat):
