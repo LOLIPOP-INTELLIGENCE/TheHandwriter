4d498115980356ec65a3f8105311c3187b2c2933
Aditya-A-garwal <aditya.agarwal030504@gmail.com>
2021-06-28 16:34:02 +0530

    feat: Adds handwriter.in to allowed hosts

--- handwriter_test/handwriter_test/settings.py
+++ handwriter_test/handwriter_test/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-b5fd-7!1myx6w1+@b-1m58!48^bfk$evjy6o(=6b9k+h8sf4yr
 DEBUG = True
 
 # ALLOWED_HOSTS = ['192.168.1.3','127.0.0.1', '172.31.6.0']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'handwriter.in', 'www.handwriter.in']
 
 
 
