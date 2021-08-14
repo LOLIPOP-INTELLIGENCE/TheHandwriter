2cd3f4af40a6bfab9bbaec96a0bb9f51ce365ebf
Aditya-A-garwal <aditya.agarwal030504@gmail.com>
2021-08-14 17:36:58 +0530

    Update setup shell script

--- setup.sh
+++ setup.sh
@@ -13,11 +13,11 @@ cd ./handwriter_test
 
 mkdir ./handwriter_test/media
 mkdir ./handwriter_test/media/AllHandwritings
+mkdir ./handwriter_test/media/text_files
 
 # Run collectstatic command
-python3 ./manage.py collectstatic
+# python3 ./manage.py collectstatic
 
 # Creates database tables
 python3 ./manage.py makemigrations
 python3 ./manage.py migrate
-
