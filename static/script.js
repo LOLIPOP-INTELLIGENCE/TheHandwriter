1c3fc13126ff4ad53599dfd14a8433bc2e460f23
Manas <manaslegodesigns@gmail.com>
2022-04-11 09:48:56 +0530

    refactor script.js

--- static/script.js
+++ static/script.js
@@ -41,30 +41,30 @@ function contactClick() {
 
 // Swiper Configuration
 var swiper = new Swiper(".swiper-container", {
-    slidesPerView: 1.5,
-    spaceBetween: 10,
-    centeredSlides: true,
-    freeMode: true,
-    grabCursor: true,
-    loop: true,
+    slidesPerView:          1.5,
+    spaceBetween:           10,
+    centeredSlides:         true,
+    freeMode:               true,
+    grabCursor:             true,
+    loop:                   true,
     pagination: {
-        el: ".swiper-pagination",
-        clickable: true
+        el:                     ".swiper-pagination",
+        clickable:              true
     },
     autoplay: {
-        delay: 4000,
-        disableOnInteraction: false
+        delay:                  4000,
+        disableOnInteraction:   false
     },
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
+        nextEl:                 ".swiper-button-next",
+        prevEl:                 ".swiper-button-prev"
     },
     breakpoints: {
         500: {
-            slidesPerView: 1
+            slidesPerView:          1
         },
         700: {
-            slidesPerView: 1.5
+            slidesPerView:          1.5
         }
     }
 });
