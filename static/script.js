b9577d01c42564e2483b5deb42776405e7679058
Samarth P <samarthprakash8@gmail.com>
2022-04-11 09:30:44 +0530

    refactor: change indentation to 4 spaces and add slider script to script.js

--- static/script.js
+++ static/script.js
@@ -1,41 +1,34 @@
-
-function nextClickScroll ()
-{
-    resSection = document.getElementById ("choose-handwriting");
-    resSection.scrollIntoView ()
+function nextClickScroll() {
+    resSection = document.getElementById("choose-handwriting");
+    resSection.scrollIntoView()
 
     // document.body.scrollTop = 10; // For Safari
     // document.documentElement.scrollTop = 10; // For Chrome, Firefox, IE and Opera
 }
 
-function userUploadHw ()
-{
-    resSection = document.getElementById ("user-upload-hw");
-    resSection.scrollIntoView ();
+function userUploadHw() {
+    resSection = document.getElementById("user-upload-hw");
+    resSection.scrollIntoView();
 }
 
-function selectDefaultHw ()
-{
-    resSection = document.getElementById ("select-default-hw");
-    resSection.scrollIntoView ();
+function selectDefaultHw() {
+    resSection = document.getElementById("select-default-hw");
+    resSection.scrollIntoView();
 }
 
-function userUploadHwNext ()
-{
-    resSection = document.getElementById ("generate");
-    resSection.scrollIntoView ();
+function userUploadHwNext() {
+    resSection = document.getElementById("generate");
+    resSection.scrollIntoView();
 }
 
-function aboutUsClick ()
-{
+function aboutUsClick() {
     resSection = document.getElementById("meet-the-team");
-    resSection.scrollIntoView ();
+    resSection.scrollIntoView();
 }
 
-function contactClick ()
-{
+function contactClick() {
     resSection = document.getElementById("contact-us");
-    resSection.scrollIntoView ();
+    resSection.scrollIntoView();
 }
 
 // function chooseFile ()
@@ -45,3 +38,33 @@ function contactClick ()
 
 //     loc.appendChild (<p style="color: white; font-family: poppins;">Filename selected succesfully</p>);
 // }
+
+// Swiper Configuration
+var swiper = new Swiper(".swiper-container", {
+    slidesPerView: 1.5,
+    spaceBetween: 10,
+    centeredSlides: true,
+    freeMode: true,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    breakpoints: {
+        500: {
+            slidesPerView: 1
+        },
+        700: {
+            slidesPerView: 1.5
+        }
+    }
+});
