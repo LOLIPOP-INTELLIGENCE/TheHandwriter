35eea6863c7b256f64518531fe2f3ed3f2dbd3a0
Aditya-A-garwal <aditya.agarwal030504@gmail.com>
2022-04-12 13:12:53 +0530

    feat: restore slider
    Signed-off-by: Aditya-A-garwal <aditya.agarwal030504@gmail.com>

--- static/script.js
+++ static/script.js
@@ -2,9 +2,6 @@
 function nextClickScroll() {
     resSection = document.getElementById("choose-handwriting");
     resSection.scrollIntoView()
-
-    // document.body.scrollTop = 10; // For Safari
-    // document.documentElement.scrollTop = 10; // For Chrome, Firefox, IE and Opera
 }
 
 function userUploadHw() {
@@ -31,14 +28,6 @@ function contactClick() {
     resSection = document.getElementById("contact-us");
     resSection.scrollIntoView();
 }
-
-// function chooseFile ()
-// {
-
-//     var loc = document.getElementById ("hw-upload-form");
-
-//     loc.appendChild (<p style="color: white; font-family: poppins;">Filename selected succesfully</p>);
-// }
 
 // Swiper Configuration
 var swiper = new Swiper(".swiper-container", {
