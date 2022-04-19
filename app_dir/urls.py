ff6df4ac5bb1219a39a6f686b08f9dfd36071ad6
Aditya-A-garwal <aditya.agarwal030504@gmail.com>
2022-04-19 10:25:32 +0530

    fix: remove unused URLS
    Signed-off-by: Aditya-A-garwal <aditya.agarwal030504@gmail.com>

--- app_dir/urls.py
+++ app_dir/urls.py
@@ -7,7 +7,5 @@ urlpatterns = [
     path('', lambda req : render( req, "hm.html" ), name='hm'),
     path('res', views.serveImgPostReq, name = 'res' ),
     path('result/<slug:path>', views.renderResult, name = 'result'),
-    path('add', views.add, name='add'),
     path('own_handwriting', lambda req : render( req, "io.html" ), name='own_handwriting'),
-    path('upload', views.upload, name='upload'),
 ]
